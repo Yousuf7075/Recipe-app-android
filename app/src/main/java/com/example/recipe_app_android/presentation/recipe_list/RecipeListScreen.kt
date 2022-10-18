66767b39ee979d145d168db69ca47c67fed3dc80
yousufaits22 <yousufaits22@gmail.com>
2022-10-18 12:59:16 +0600

    recipe list: design item & styling

--- app/src/main/java/com/example/recipe_app_android/presentation/recipe_list/RecipeListScreen.kt
+++ app/src/main/java/com/example/recipe_app_android/presentation/recipe_list/RecipeListScreen.kt
@@ -1,9 +1,6 @@
 package com.example.recipe_app_android.presentation.recipe_list
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
@@ -16,6 +13,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.recipe_app_android.presentation.recipe_list.components.RecipeListItem
 
 @Composable
 fun RecipeListScreen(
@@ -24,21 +22,15 @@ fun RecipeListScreen(
 ) {
     val state = viewModel.state.value
     Box(modifier = Modifier.fillMaxSize()) {
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
-            items(state.recipes){ recipe ->
-
-                Text(
-                    text = recipe.title,
-                    color = MaterialTheme.colors.onPrimary,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 20.dp)
-                        .align(Alignment.Center)
-                )
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+        ) {
+            items(state.recipes) { recipe ->
+                RecipeListItem(recipe = recipe)
             }
         }
-        if(state.error.isNotBlank()) {
+        if (state.error.isNotBlank()) {
             Text(
                 text = state.error,
                 color = MaterialTheme.colors.error,
@@ -49,7 +41,7 @@ fun RecipeListScreen(
                     .align(Alignment.Center)
             )
         }
-        if(state.isLoading) {
+        if (state.isLoading) {
             CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
         }
     }
--- app/src/main/java/com/example/recipe_app_android/presentation/recipe_list/components/RecipeListItem.kt
+++ app/src/main/java/com/example/recipe_app_android/presentation/recipe_list/components/RecipeListItem.kt
@@ -1,2 +1,44 @@
 package com.example.recipe_app_android.presentation.recipe_list.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.recipe_app_android.domain.models.Recipe
+
+
+@Composable
+fun RecipeListItem(recipe: Recipe) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+    ) {
+        Column() {
+            AsyncImage(
+                model = recipe.image,
+                contentDescription = "recipe image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(50f)
+                    .background(
+                        Color.Cyan
+                    )
+            )
+            Text(
+                text = recipe.title,
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp)
+            )
+        }
+    }
+}
+
